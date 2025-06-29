84fa5bf2caedd1b8fd1f8a0ef6c1f50757d4800e
Arslan Iqbal Ihsan <121037832+arslaniqbalihsan@users.noreply.github.com>
2025-06-29 15:55:24 +0200

    Add files via upload
    TASK 6

---
+++ urls.py
@@ -0,0 +1,17 @@
+from django.urls import path
+
+from socialnetwork.views.html import timeline
+from socialnetwork.views.html import follow
+from socialnetwork.views.html import unfollow
+from socialnetwork.views.rest import PostsListApiView
+from socialnetwork.views.html import bullshitters
+
+app_name = "socialnetwork"
+
+urlpatterns = [
+    path("api/posts", PostsListApiView.as_view(), name="posts_fulllist"),
+    path("html/timeline", timeline, name="timeline"),
+    path("api/follow", follow, name="follow"),
+    path("api/unfollow", unfollow, name="unfollow"),
+    path("html/bullshitters", bullshitters, name="bullshitters"),
+]
